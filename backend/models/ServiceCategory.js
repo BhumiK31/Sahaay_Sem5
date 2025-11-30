750631229cd6a50219725f923c5ae71f079e6d5c
jagruti-24 <jagruti0044@gmail.com>
2025-11-30 12:25:32 +0530

    changed

--- backend/models/ServiceCategory.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const categorySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: String,
-  tags: [String],
-});
-
-module.exports = mongoose.model('ServiceCategory', categorySchema);
